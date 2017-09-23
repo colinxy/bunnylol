ed8a2b079161cefe54286189506f560b0ad8733a
Xinyu Yang <colin.yangxinyu@outlook.com>
2017-09-23 13:06:03 -0700

    add color logging

--- bunnylol/__main__.py
+++ bunnylol/__main__.py
@@ -2,17 +2,34 @@
 Run application in debug mode
 """
 
+import logging
+import colorlog
+
 from .bunnylol import make_app
 
 from aiohttp import web
 import aiohttp_debugtoolbar
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+# ch = logging.StreamHandler()
+# formatter = logging.Formatter(
+#     '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+# )
+ch = colorlog.StreamHandler()
+formatter = colorlog.ColoredFormatter(
+    '%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 
 app = make_app(
     debug=True,
     db_configs={
+        # 'dialect': 'sqlite',
         'dsn': 'sqlite:///bunnylol.db',
-        'echo': True,
+        'echo': False,
     },
 )
 aiohttp_debugtoolbar.setup(app, intercept_redirects=False)
